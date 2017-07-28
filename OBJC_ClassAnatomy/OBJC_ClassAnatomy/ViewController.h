61e5bddd39f80b57a3ac0ec0cea74791caa6de24
Sattar Hummatli <settarxan@gmail.com>
2017-07-28 11:07:47 +0400

    Played with Objective-C ClassAnatomy, how it defines and implements

---
+++ OBJC_ClassAnatomy/OBJC_ClassAnatomy/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  OBJC_ClassAnatomy
+//
+//  Created by Settar Hummetli on 7/28/17.
+//  Copyright © 2017 Settar Hummetli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (nonatomic, strong) NSString *name;
+
+@end
+
---
+++ OBJC_ClassAnatomy/OBJC_ClassAnatomy/ViewController.m
@@ -0,0 +1,43 @@
+//
+//  ViewController.m
+//  OBJC_ClassAnatomy
+//
+//  Created by Settar Hummetli on 7/28/17.
+//  Copyright © 2017 Settar Hummetli. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property (nonatomic, strong) NSString *vehicle;
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"Name: %@", self.name);
+    
+    self.name = @"Sattar";
+    
+    NSLog(@"Name: %@", self.name);
+    
+    _name = @"Peter";
+    
+    NSLog(@"Name: %@", _name);
+    
+    [self setName:@"Mammad"];
+    
+    NSLog(@"Name: %@", [self name]);
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
