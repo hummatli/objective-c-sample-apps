9b561dadfd0dfe9c9c123cda04fa03934a68007c
Sattar Hummatli <settarxan@gmail.com>
2017-07-28 13:11:14 +0400

    Played around with Objectve-C's pointers

---
+++ Pointers-OBJC/Pointers-OBJC/ViewController.h
@@ -0,0 +1,21 @@
+//
+//  ViewController.h
+//  Pointers-OBJC
+//
+//  Created by Settar Hummetli on 7/28/17.
+//  Copyright © 2017 Settar Hummetli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (nonatomic, strong) NSString *name;
+@property (nonatomic) int age;
+
+@property (nonatomic) NSInteger debt;
+@property (nonatomic) NSNumber *bankBalance;
+
+
+@end
+
---
+++ Pointers-OBJC/Pointers-OBJC/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  Pointers-OBJC
+//
+//  Created by Settar Hummetli on 7/28/17.
+//  Copyright © 2017 Settar Hummetli. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.name = [[NSString alloc]init];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
